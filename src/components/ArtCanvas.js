2fed421b06fbb5e29109a1e2369543b50f59efbe
Richard Marshall <missingmarshall@gmail.com>
2020-05-11 21:52:15 +0100

    added dynamic script loading to modify canvas

--- src/components/ArtCanvas.js
+++ src/components/ArtCanvas.js
@@ -1,20 +1,47 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-const ArtCanvas = () => {
+class ArtCanvas extends React.Component {
     // get the requested artwork name from the url
-    let { name } = useParams();
+    state = {
+        name: this.props.match.params.name,
+        dimension: {
+            width: this.props.width || 800,
+            height: this.props.height || 800
+        },
+        scriptPath: '/src/artwork/scripts/'
+    }
 
-    return (
-        <div>
-            <h2>
-                { name }
-            </h2>
-            <canvas>
 
-            </canvas>
-        </div>
-    );
+    renderCanvas() {
+        return <canvas width={this.state.width} height={this.state.height} />
+    }
+
+    // initialise loading of external JS script
+    componentDidMount() {        
+        this.loadExternalScript(this.state.scriptPath + this.state.name + '.js') 
+    }
+
+    loadExternalScript(filepath) {
+        console.log(filepath);
+        const script = document.createElement("script");
+        script.async = true;
+        script.src = filepath;
+        this.canvasDiv.appendChild(script);
+    }
+
+    render() {
+        console.log(this.props)
+        return (
+            // store a reference to this div within the class, to append the externam script to
+            <div ref={el => (this.canvasDiv = el)}>
+                <h2>
+                    { this.state.name }
+                </h2>
+                { this.renderCanvas() }
+            </div>
+        );
+    }
 }
 
-export default ArtCanvas;
+export default withRouter(ArtCanvas);
