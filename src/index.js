5153022359516ada77765fe414ff22901f361aa4
Richard Marshall <missingmarshall@gmail.com>
2020-05-11 21:14:17 +0100

    added react router imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 class App extends React.Component {
     render() {
