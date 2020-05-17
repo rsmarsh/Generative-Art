238e64c7058f6c26d88a60a765ce47d542dd4135
Richard Marshall <missingmarshall@gmail.com>
2020-05-17 16:16:19 +0100

    restructured art directories and location

---
+++ src/components/artwork/cityscape.js
@@ -0,0 +1,14 @@
+const Cityscape = (ctx) => {
+
+    drawToCanvas(ctx);
+    
+};
+
+const drawToCanvas = (ctx) => {
+    console.log("renderToCanvas function");
+
+};
+
+
+
+export default Cityscape;
