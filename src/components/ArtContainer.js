9b004184272388648567d7e61886448d5aa18320
Richard Marshall <missingmarshall@gmail.com>
2020-05-18 20:46:13 +0100

    added component to wrap canvas and settings

---
+++ src/components/ArtContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ArtCanvas from './ArtCanvas';
+
+class ArtContainer extends React.Component {
+
+    render() {
+        
+        return (
+            <ArtCanvas />
+        )
+    }
+}
+
+export default ArtContainer; 
