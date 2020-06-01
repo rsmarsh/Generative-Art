16a55139949268657303fdd65c4a64ef9d603adb
Richard Marshall <missingmarshall@gmail.com>
2020-06-01 16:08:15 +0100

    moved top level App logic into a seperate file

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import ArtList from './ArtList';
+import ArtContainer from './ArtContainer';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <h1>My Projects</h1>
+                </div>
+
+                <Switch>
+                    <Route exact path="/">
+                        <ArtList />
+                    </Route>
+
+                    {/* the artwork name and dimension (2d/3d) from the url is automatically passed to the component */}
+                    <Route exact path="/art/:dimensional/:name">
+                        <ArtContainer />
+                    </Route>
+                </Switch>
+            </Router>
+        )
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import ArtList from './components/ArtList';
-import ArtContainer from './components/ArtContainer';
+import App from './components/App';
 
-class App extends React.Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <h1>My Projects</h1>
-                </div>
 
-                <Switch>
-                    <Route exact path="/">
-                        <ArtList />
-                    </Route>
 
-                    {/* the artwork name and dimension (2d/3d) from the url is automatically passed to the component */}
-                    <Route exact path="/art/:dimensional/:name">
-                        <ArtContainer />
-                    </Route>
-                </Switch>
-            </Router>
-        )
-    }
-}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
