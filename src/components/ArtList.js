203ed235354bc87d3330bb5281a9086845e4a7f6
unknown <missingmarshall@gmail.com>
2020-10-31 01:43:52 +0000

    converted to functional components

--- src/components/ArtList.js
+++ src/components/ArtList.js
@@ -3,42 +3,29 @@ import ArtTile from './ArtTile';
 
 import artworkList from './artwork/artwork-list.json';
 
-
-
-class ArtList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            artworkList: artworkList
-        }
-
-    }
-
-    componentDidMount() {
-
-    }
-
-    getFilepath(dimensional, name) {
-        if (dimensional === "2d") {
-            return `/art/${dimensional}/${name}`;
-        }
-
-        if (dimensional === "3d") {
-            //TODO: add 3d support
-        }
-    }
-
-    renderTiles() {
-        return artworkList.twoDimensional.map(artwork => {
-            return <ArtTile title={artwork.title} description={artwork.description} path={this.getFilepath('2d', artwork.title)} key={artwork.title} />;
-        });
+const getFilepath = (dimensional, name) => {
+    if (dimensional === "2d") {
+        return `/art/${dimensional}/${name}`;
     }
     
-    render() {
-        return (
-            this.renderTiles()
-        )
+    if (dimensional === "3d") {
+        //TODO: add 3d support
     }
+}
+
+const renderTiles = () => {
+    return artworkList.twoDimensional.map(artwork => {
+        return <ArtTile title={artwork.title} description={artwork.description} path={getFilepath('2d', artwork.title)} key={artwork.title} />;
+    });
+}
+
+const ArtList = () => {
+    return (
+        <div>
+            {renderTiles()}
+        </div>
+    );
+    
 }
 
 export default ArtList;
--- src/index.js
+++ src/index.js
@@ -4,27 +4,25 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ArtList from './components/ArtList';
 import ArtContainer from './components/ArtContainer';
 
-class App extends React.Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <h1>My Projects</h1>
-                </div>
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <h1>Generative Art</h1>
+            </div>
 
-                <Switch>
-                    <Route exact path="/">
-                        <ArtList />
-                    </Route>
+            <Switch>
+                <Route exact path="/">
+                    <ArtList />
+                </Route>
 
-                    {/* the artwork name and dimension (2d/3d) from the url is automatically passed to the component */}
-                    <Route exact path="/art/:dimensional/:name">
-                        <ArtContainer />
-                    </Route>
-                </Switch>
-            </Router>
-        )
-    }
-}
+                {/* the artwork name and dimension (2d/3d) from the url is automatically passed to the component */}
+                <Route exact path="/art/:dimensional/:name">
+                    <ArtContainer />
+                </Route>
+            </Switch>
+        </Router>
+    )
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
