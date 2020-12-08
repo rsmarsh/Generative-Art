af7006ecbebd5d250c614bf7522d9e29cc247aca
Richard Marshall <missingmarshall@gmail.com>
2020-12-08 20:40:45 +0000

    render seed menu to page, pass ref to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ArtList from './ArtList';
 import ArtContainer from './ArtContainer';
+import FloatMenu from './FloatMenu';
 
 import './App.css';
 
+
 const App = () => {
+
+    const [seed, setSeed] = useState('rsmarsh');
+
     return (
         <Router>
+            <FloatMenu seed={seed} handleSeedChange={setSeed} />
             <div>
                 <h1>Generative Art</h1>
             </div>
@@ -19,7 +25,7 @@ const App = () => {
 
                 {/* the artwork name and dimension (2d/3d) from the url is automatically passed to the component */}
                 <Route exact path="/art/:dimensional/:name">
-                    <ArtContainer />
+                    <ArtContainer seed={seed} />
                 </Route>
             </Switch>
         </Router>
