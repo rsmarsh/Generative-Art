a8ea3e0c7a5589e6f9cd51b03e4a27fc3b755892
Richard Marshall <missingmarshall@gmail.com>
2020-12-08 22:50:00 +0000

    seed input now a controlled input

--- src/components/FloatMenu.js
+++ src/components/FloatMenu.js
@@ -1,13 +1,17 @@
 import React, { useContext, useState } from 'react';
 import SeedContext from './App';
 
-const FloatMenu = () => {
+const FloatMenu = (props) => {
     console.log(SeedContext);
     return (
-        <div class="float-menu">
+        <div className="float-menu">
             <div>
                 <h2>Seed:</h2>
-                <input type="text"/>
+                <input 
+                    type="text" 
+                    value={props.seed}
+                    onChange={(e) => {props.handleSeedChange(e.target.value)}}
+                />
             </div>
             <button><span role="img" aria-label="change seed">🌱</span></button>
         </div>
