3ba9bced2ef24b88c515eb1a629f0c2a4a6ec590
rsmarsh <missingmarshall@gmail.com>
2022-01-02 17:36:05 +0000

    added a back button to allow home navigation

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,9 @@ import ArtContainer from './ArtContainer';
 import FloatMenu from './FloatMenu';
 import { getSeed, storeSeed } from '../data/session.js';
 
-import './App.css';
+import forwardIcon from '../icons/forward.svg';
 
+import './App.scss';
 
 const App = () => {
 
@@ -18,21 +19,30 @@ const App = () => {
     }, [seed]);
 
 
+    const renderHeading = (showBackButton) => {
+        return (
+            <div className="page-heading">
+                {showBackButton && <a className="back-button" href="/">{<img alt="go back" src={forwardIcon}/>}</a> }
+                <h1 className="page-title">Generative JavaScript Art</h1>
+            </div>
+        )
+    }
+
     return (
         <Router>
-            <div>
-                <h1>Generative Art</h1>
-            </div>
+            
+            <FloatMenu seed={seed} handleSeedChange={setSeed} />
 
             <Switch>
                 <Route exact path="/">
+                    {renderHeading(false)}
                     <ArtList />
                 </Route>
 
                 {/* the artwork name and dimension (2d/3d) from the url is automatically passed to the component */}
                 <Route exact path="/art/:dimensional/:name">
+                    {renderHeading(true)}
                     <ArtContainer seed={seed} />
-                    <FloatMenu seed={seed} handleSeedChange={setSeed} />
                 </Route>
             </Switch>
         </Router>
